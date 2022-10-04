9afc2760de5b11fbbc12560ad0ab505e387fb214
MZC01-DGCHOI <dgchoi@mz.co.kr>
2022-10-04 20:59:58 +0900

    Create JPA dependency & Posts Class

---
+++ src/main/java/com/dlddy/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,32 @@
+package com.dlddy.book.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter // 클래스 내 모든 필드의 Getter 메소드를 자동 생성
+@NoArgsConstructor // 기본 생성자 자동 추가 -> public Posts() {}와 같은 효과
+@Entity //테이블과 링크될 클래스임을 나타냄, 카멜케이스 -> 언더스코어 네이밍(_)으로 테이블 이름 매칭 (SalesManager.java -> sales_manager table)
+public class Posts {
+
+    @Id //해당 테이블의 PK 필드를 나타냄
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //PK의 생성 규칙을 나타냄, GenerationType.IDENTITY 추가해야 auto_increment 된다.
+    private Long id;
+
+    @Column(length = 500, nullable = false) //테이블의 칼럼. 선언하지 않아도 해당 클래스의 필드는 모두 칼럼이 된다. 사용 하는이유: 기본값 외의 추가로 변경이 필요한 옵션이 있을 때
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder //해당 클래스의 빌더 패턴 클래스를 생성. 생성자 상단에 선언 시 생성자에 포함된 필드만 빌더에 포함
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
--- src/test/java/com/dlddy/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/dlddy/book/springboot/web/HelloControllerTest.java
@@ -8,9 +8,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class) //테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행시킨다. SpringRunner라는 스프링 실행자를 사용. 스프링부트테스트-JUnit 연결자 역할
 @WebMvcTest(controllers = HelloController.class) //Web(Spring MVC)에 집중할 수 있는 어노테이션
@@ -27,5 +27,19 @@ public class HelloControllerTest {
                 .andExpect(status().isOk()) //mvc.perform의 결과를 검증합니다. HTTP Header의 statu가 200인지 아닌지 검증
                 .andExpect(content().string(hello)); //Controller에서 "hello"를 리턴하는게 맞는지 검증
     }
+
+    @Test
+    public void returnHelloDto() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                .param("name", name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", equalTo(name))) //"{ name : ""}"
+                .andExpect(jsonPath("$.amount", equalTo(amount)));
+    }
+
 
 }
